8a9c7e3d9799b7eb109e1f6c3b2a82813c6f8bb3
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 17:16:32 +0700

    menambahkan class FanLaptop

---
+++ FanLaptop.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public enum fanState
+{
+    Quiet, Turbo, Balanced, Performance
+}
+public class FanLaptop
+{
+    private fanState state = fanState.Quiet;
+
+    public void turbo()
+    }
+}
--- Main.cs
+++ Main.cs
@@ -14,5 +14,61 @@
         Console.WriteLine("Monitor " + kode.getKodeProduk("Monitor"));
         Console.WriteLine("Smartwatch " + kode.getKodeProduk("Smartwatch"));
         Console.WriteLine("Kamera " + kode.getKodeProduk("Kamera"));
+
+        FanLaptop fan = new FanLaptop();
+        Console.WriteLine("========== State Fan Laptop ==========");
+        Console.WriteLine("========== STATUS AWAL : QUIET ==========");
+        
+        bool status = true;
+        while (true)
+        {
+            string choice = Console.ReadLine();
+            fanState state = fanState.Quiet;
+            switch (state)
+            {
+                case fanState.Quiet:
+                    if (choice == "Turbo")
+                    {
+                        state = fanState.Turbo;
+                        Console.WriteLine("Fan Quiet berubah menjadi Turbo");
+                    }
+                    else if (choice == "Balanced")
+                    {
+                        state = fanState.Balanced;
+                        Console.WriteLine("Fan Quiet berubah menjadi Balanced");
+                    }
+                    return;
+                case fanState.Turbo:
+                    if (choice == "Quiet")
+                    {
+                        state = fanState.Quiet;
+                        Console.WriteLine("Fan Turbo berubah menjadi Quiet");
+                    }
+                    else if (choice == "Performance")
+                    {
+                        state = fanState.Performance;
+                        Console.WriteLine("Fan Turbo berubah menjadi Performance");
+                    }
+                    return;
+                case fanState.Performance:
+                    if (choice == "Turbo")
+                    {
+                        state = fanState.Turbo;
+                        Console.WriteLine("Fan Performance berubah menjadi Turbo");
+                    } else if (choice == "Balanced")
+                    {
+                        state = fanState.Balanced;
+                        Console.WriteLine("Fan Performance berubah menjadi Balanced");
+                    }
+                    return;
+                case fanState.Balanced:
+                    if (choice == "Performance")
+                    {
+                        state = fanState.Performance;
+                        Console.WriteLine("Fan Balanced berubah menjadi Performance");
+                    }
+                    return;
+            }
+        }
     }
 }
