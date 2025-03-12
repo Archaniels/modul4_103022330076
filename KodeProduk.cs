91c31ef8cc378403649688de557c8273713a16e7
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 16:09:22 +0700

    menambahkan class KodeProduk

---
+++ KodeProduk.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public class KodeProduk
+{
+    private static Dictionary<string, string> kode2 = new Dictionary<string, string>
+        {
+            {"Laptop", "E100"},
+            {"Smartphone", "E101"},
+            {"Tablet", "E102"},
+            {"Headset", "E103"},
+            {"Keyboard", "E104"},
+            {"Mouse", "E105"},
+            {"Printer", "E106"},
+            {"Monitor", "E107"},
+            {"Smartwatch", "E108"},
+            {"Kamera", "E109"},
+        };
+
+    public string getKodeProduk(string produk)
+    {
+        if (kode2.ContainsKey(produk))
+        {
+            return kode2[produk];
+        }
+        return null;
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        KodeProduk kode = new KodeProduk();
+        Console.WriteLine("Produk Elektronik ----- Kode Produk");
+        Console.WriteLine("Laptop " + kode.getKodeProduk("Laptop"));
+        Console.WriteLine("Smartphone " + kode.getKodeProduk("Smartphone"));
+        Console.WriteLine("Tablet " + kode.getKodeProduk("Tablet"));
+        Console.WriteLine("Headset " + kode.getKodeProduk("Headset"));
+        Console.WriteLine("Keyboard " + kode.getKodeProduk("Keyboard"));
+        Console.WriteLine("Mouse " + kode.getKodeProduk("Mouse"));
+        Console.WriteLine("Printer " + kode.getKodeProduk("Printer"));
+        Console.WriteLine("Monitor " + kode.getKodeProduk("Monitor"));
+        Console.WriteLine("Smartwatch " + kode.getKodeProduk("Smartwatch"));
+        Console.WriteLine("Kamera " + kode.getKodeProduk("Kamera"));
+    }
+}
